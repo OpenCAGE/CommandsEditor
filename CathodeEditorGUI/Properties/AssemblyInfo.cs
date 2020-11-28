00988fcd58fe600d72d2cfaf12d3afa00440de56
MattFiler <matthew.filer@hotmail.co.uk>
2020-11-28 15:52:24 +0000

    Updated build info

--- CathodeEditorGUI/Properties/AssemblyInfo.cs
+++ CathodeEditorGUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alien Isolation Script Editor")]
+[assembly: AssemblyTitle("OpenCAGE - Cathode Script Editor")]
 [assembly: AssemblyDescription("A tool to browse and edit Alien: Isolation's COMMANDS.PAK scripting files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matt Filer")]
-[assembly: AssemblyProduct("CathodeEditorGUI")]
+[assembly: AssemblyProduct("OpenCAGE - Cathode Script Editor")]
 [assembly: AssemblyCopyright("Copyright © Matt Filer 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
