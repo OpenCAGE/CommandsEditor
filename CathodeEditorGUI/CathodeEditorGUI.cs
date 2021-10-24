07bfef4987fcb9f616e2fbda0a2d3914d08f4667
MattFiler <matthew.filer@hotmail.co.uk>
2021-10-24 16:46:34 +0100

    Don't remove param if there are none

--- CathodeEditorGUI/CathodeEditorGUI.cs
+++ CathodeEditorGUI/CathodeEditorGUI.cs
@@ -429,6 +429,7 @@ namespace CathodeEditorGUI
         private void removeParameter_Click(object sender, EventArgs e)
         {
             if (selected_node == null) return;
+            if (selected_node.parameters.Count == 0) return;
             CathodeEditorGUI_RemoveParameter remove_parameter = new CathodeEditorGUI_RemoveParameter(selected_node);
             remove_parameter.Show();
             remove_parameter.FormClosed += new FormClosedEventHandler(param_remove_closed);
