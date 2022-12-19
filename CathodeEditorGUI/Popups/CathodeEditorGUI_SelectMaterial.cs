225cbeed5332ef5d093d3ee3f0aae01285022289
MattFiler <matthew.filer@hotmail.co.uk>
2022-12-19 23:53:10 +0000

    sort material list

--- CathodeEditorGUI/Popups/CathodeEditorGUI_SelectMaterial.cs
+++ CathodeEditorGUI/Popups/CathodeEditorGUI_SelectMaterial.cs
@@ -15,23 +15,48 @@ namespace CathodeEditorGUI.Popups
         public int SelectedMaterialIndex = -1;
         public int MaterialIndexToEdit = -1;
 
+        List<ListedMaterial> _materials = new List<ListedMaterial>();
+
         public CathodeEditorGUI_SelectMaterial(int materialIndexToEdit, int defaultMaterialIndex = -1)
         {
             InitializeComponent();
 
-            for (int i = 0; i < CurrentInstance.materialDB.MaterialNames.Count; i++)
-            {
-                materialList.Items.Add(CurrentInstance.materialDB.MaterialNames[i]);
-            }
-
             MaterialIndexToEdit = materialIndexToEdit;
-            if (defaultMaterialIndex != -1) materialList.SelectedIndex = defaultMaterialIndex;
+            for (int i = 0; i < CurrentInstance.materialDB.MaterialNames.Count; i++)
+                _materials.Add(new ListedMaterial(CurrentInstance.materialDB.MaterialNames[i], i));
+            _materials.Sort();
+            _materials.Reverse();
+
+            for (int i = 0; i < _materials.Count; i++)
+            {
+                materialList.Items.Add(_materials[i].MaterialName);
+                if (materialIndexToEdit != -1 && _materials[i].Index == defaultMaterialIndex)
+                    materialList.SelectedIndex = i;
+            }
         }
 
         private void selectMaterial_Click(object sender, EventArgs e)
         {
-            SelectedMaterialIndex = materialList.SelectedIndex;
+            SelectedMaterialIndex = _materials[materialList.SelectedIndex].Index;
             this.Close();
+        }
+
+        private class ListedMaterial : IComparable
+        {
+            public ListedMaterial(string name, int index)
+            {
+                MaterialName = name;
+                Index = index;
+            }
+
+            public string MaterialName;
+            public int Index;
+
+            public int CompareTo(object obj)
+            {
+                if (!(obj is ListedMaterial)) return 0;
+                return string.Compare(((ListedMaterial)obj).MaterialName, MaterialName);
+            }
         }
     }
 }
