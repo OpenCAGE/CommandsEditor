47917ffa3d9d59b61fb7a1998d6189f524f86cfe
Matt Filer <matthew.filer@hotmail.co.uk>
2025-11-02 18:07:37 +0000

    split material creation into a separate class, add more features

--- CathodeEditorGUI/Popups/UserControls/GUI_ModelViewer.xaml.cs
+++ CathodeEditorGUI/Popups/UserControls/GUI_ModelViewer.xaml.cs
@@ -5,6 +5,7 @@ using CATHODE.Scripting;
 using CATHODE.ShaderTypes;
 using CathodeLib;
 using CommandsEditor.DockPanels;
+using CommandsEditor.Scripts;
 using OpenCAGE;
 using System;
 using System.Collections.Generic;
@@ -59,121 +60,7 @@ namespace CommandsEditor.Popups.UserControls
             {
                 Materials.Material material = Content.resource.materials.GetAtWriteIndex(materialIndex == -1 ? submesh.MaterialIndex : materialIndex);
                 Shaders.Shader shader = Content.resource.shaders.Entries[material.ShaderIndex];
-
-                int diffuseMap = -1;
-                int diffuseUvMult = -1;
-                switch (shader.Ubershader)
-                {
-                    case SHADER_LIST.CA_ENVIRONMENT:
-                        diffuseMap = (int)CA_ENVIRONMENT.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_ENVIRONMENT.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_DECAL_ENVIRONMENT:
-                        diffuseMap = (int)CA_DECAL_ENVIRONMENT.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_DECAL_ENVIRONMENT.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_CHARACTER:
-                        diffuseMap = (int)CA_CHARACTER.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_CHARACTER.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_SKIN:
-                        diffuseMap = (int)CA_SKIN.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_SKIN.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_HAIR:
-                        diffuseMap = (int)CA_HAIR.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_HAIR.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_EYE:
-                        diffuseMap = (int)CA_EYE.SAMPLERS.IRIS_MAP;
-                        break;
-                    case SHADER_LIST.CA_SKIN_OCCLUSION:
-                        diffuseMap = (int)CA_SKIN_OCCLUSION.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_SKIN_OCCLUSION.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_DECAL:
-                        diffuseMap = (int)CA_DECAL.SAMPLERS.DIFFUSE_MAP;
-                        break;
-                    case SHADER_LIST.CA_FOGPLANE:
-                        diffuseMap = (int)CA_FOGPLANE.SAMPLERS.DIFFUSE_MAP_0;
-                        break;
-                    case SHADER_LIST.CA_EFFECT:
-                        diffuseMap = (int)CA_EFFECT.SAMPLERS.DIFFUSE_MAP_0;
-                        break;
-                    case SHADER_LIST.CA_LIQUID_ENVIRONMENT:
-                        diffuseMap = (int)CA_LIQUID_ENVIRONMENT.SAMPLERS.LIQUIFLOW_DISTORTION_MAP;
-                        break;
-                    case SHADER_LIST.CA_LIQUID_CHARACTER:
-                        diffuseMap = (int)CA_LIQUID_CHARACTER.SAMPLERS.LIQUIFLOW_DISTORTION_MAP;
-                        break;
-                    case SHADER_LIST.CA_SKYDOME:
-                        diffuseMap = (int)CA_SKYDOME.SAMPLERS.SKYDOME_MAP;
-                        break;
-                    case SHADER_LIST.CA_SURFACE_EFFECTS:
-                        diffuseMap = (int)CA_SURFACE_EFFECTS.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_SURFACE_EFFECTS.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_EFFECT_OVERLAY:
-                        diffuseMap = (int)CA_EFFECT_OVERLAY.SAMPLERS.TEXTURE_MAP;
-                        break;
-                    case SHADER_LIST.CA_TERRAIN:
-                        diffuseMap = (int)CA_TERRAIN.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_TERRAIN.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_PLANET:
-                        diffuseMap = (int)CA_PLANET.SAMPLERS.TERRAIN_MAP;
-                        diffuseUvMult = (int)CA_PLANET.PARAMETERS.TERRAIN_MAP_UV_SCALE;
-                        break;
-                    case SHADER_LIST.CA_LIGHTMAP_ENVIRONMENT:
-                        diffuseMap = (int)CA_LIGHTMAP_ENVIRONMENT.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_LIGHTMAP_ENVIRONMENT.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_STREAMER:
-                        diffuseMap = (int)CA_STREAMER.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_STREAMER.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_LOW_LOD_CHARACTER:
-                        diffuseMap = (int)CA_LOW_LOD_CHARACTER.SAMPLERS.DIFFUSE_MAP;
-                        diffuseUvMult = (int)CA_LOW_LOD_CHARACTER.PARAMETERS.DIFFUSE_UV_MULT;
-                        break;
-                    case SHADER_LIST.CA_CAMERA_MAP:
-                        diffuseMap = (int)CA_CAMERA_MAP.SAMPLERS.DIFFUSE_MAP;
-                        break;
-                }
-                if (diffuseMap != -1)
-                {
-                    if (shader.SamplerRemaps.Count > diffuseMap)
-                    {
-                        int diffuseMapIndex = shader.SamplerRemaps[diffuseMap];
-                        if (diffuseMapIndex != 255)
-                        {
-                            TexturePtr ptr = material.TextureReferences[diffuseMapIndex];
-                            if (ptr != null && ptr.Index != -1)
-                            {
-                                Textures tex = ptr.Location == TexturePtr.Source.GLOBAL ? Singleton.GlobalTextures : Content.resource.textures;
-                                Textures.TEX4 diff = tex.GetAtWriteIndex(ptr.Index);
-                                byte[] diffDDS = diff?.ToDDS();
-                                ImageBrush brush = new ImageBrush(diffDDS?.ToBitmap()?.ToImageSource());
-
-                                float uvScale = 1.0f;
-                                if (diffuseUvMult != -1)
-                                {
-                                    if (shader.PixelShaderParameterRemaps.Count > diffuseUvMult)
-                                    {
-                                        if (shader.PixelShaderParameterRemaps[diffuseUvMult] != 255)
-                                        {
-                                            uvScale = material.PixelShaderConstants[shader.PixelShaderParameterRemaps[diffuseUvMult]];
-                                        }
-                                    }
-                                }
-                                brush.Transform = new ScaleTransform(uvScale, uvScale);
-
-                                DiffuseMaterial mat = new DiffuseMaterial(brush);
-                                submeshGeo.Material = mat;
-                            }
-                        }
-                    }
-                }
+                MaterialApplier.ApplyMaterial(submeshGeo, material, shader, Content.resource.textures);
             }
 
             //Get transform data
