f8115ea14f40b1f5a0469061e3c5da3a81b28fab
Matt Filer <matthew.filer@hotmail.co.uk>
2026-04-11 18:25:38 +0100

    tweak global constants editor

--- CathodeEditorGUI/Popups/Configuration Editors/GlobalConstantsEditor.Designer.cs
+++ CathodeEditorGUI/Popups/Configuration Editors/GlobalConstantsEditor.Designer.cs
@@ -29,14 +29,11 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GlobalConstantsEditor));
-            this.label6 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label34 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -49,6 +46,10 @@
             this.stealth_light_meter_full_light_threshold = new System.Windows.Forms.NumericUpDown();
             this.stealth_light_meter_full_dark_threshold = new System.Windows.Forms.NumericUpDown();
             this.stealth_light_meter_timeout_when_detected = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.interaction_distance_threshold)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.min_time_between_squad_shots_lower_bound)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.min_time_between_squad_shots_upper_bound)).BeginInit();
@@ -57,22 +58,16 @@
             ((System.ComponentModel.ISupportInitialize)(this.stealth_light_meter_full_light_threshold)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.stealth_light_meter_full_dark_threshold)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.stealth_light_meter_timeout_when_detected)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 9);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(130, 20);
-            this.label6.TabIndex = 314;
-            this.label6.Text = "Suspicious Items";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(20, 31);
+            this.label10.Location = new System.Drawing.Point(10, 21);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(202, 13);
             this.label10.TabIndex = 315;
@@ -81,26 +76,16 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(79, 50);
+            this.label9.Location = new System.Drawing.Point(69, 40);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(77, 13);
             this.label9.TabIndex = 318;
             this.label9.Text = "Min -------> Max";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 80);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
-            this.label1.TabIndex = 319;
-            this.label1.Text = "Squads";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 102);
+            this.label2.Location = new System.Drawing.Point(10, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(159, 13);
             this.label2.TabIndex = 320;
@@ -109,7 +94,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(79, 121);
+            this.label3.Location = new System.Drawing.Point(69, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 13);
             this.label3.TabIndex = 323;
@@ -118,26 +103,16 @@
             // label34
             // 
             this.label34.AutoSize = true;
-            this.label34.Location = new System.Drawing.Point(13, 157);
+            this.label34.Location = new System.Drawing.Point(11, 21);
             this.label34.Name = "label34";
             this.label34.Size = new System.Drawing.Size(152, 13);
             this.label34.TabIndex = 345;
             this.label34.Text = "Interaction Distance Threshold";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(265, 9);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 20);
-            this.label4.TabIndex = 347;
-            this.label4.Text = "Stealth Light Meter";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(273, 31);
+            this.label5.Location = new System.Drawing.Point(10, 22);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(124, 13);
             this.label5.TabIndex = 348;
@@ -146,7 +121,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(274, 70);
+            this.label7.Location = new System.Drawing.Point(11, 61);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(99, 13);
             this.label7.TabIndex = 354;
@@ -155,7 +130,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(274, 109);
+            this.label8.Location = new System.Drawing.Point(11, 100);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(99, 13);
             this.label8.TabIndex = 356;
@@ -163,7 +138,7 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(327, 173);
+            this.btnSave.Location = new System.Drawing.Point(314, 207);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(136, 33);
             this.btnSave.TabIndex = 358;
@@ -173,7 +148,7 @@
             // 
             // interaction_distance_threshold
             // 
-            this.interaction_distance_threshold.Location = new System.Drawing.Point(23, 173);
+            this.interaction_distance_threshold.Location = new System.Drawing.Point(13, 37);
             this.interaction_distance_threshold.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -186,7 +161,7 @@
             // min_time_between_squad_shots_lower_bound
             // 
             this.min_time_between_squad_shots_lower_bound.DecimalPlaces = 3;
-            this.min_time_between_squad_shots_lower_bound.Location = new System.Drawing.Point(24, 120);
+            this.min_time_between_squad_shots_lower_bound.Location = new System.Drawing.Point(14, 38);
             this.min_time_between_squad_shots_lower_bound.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -199,7 +174,7 @@
             // min_time_between_squad_shots_upper_bound
             // 
             this.min_time_between_squad_shots_upper_bound.DecimalPlaces = 3;
-            this.min_time_between_squad_shots_upper_bound.Location = new System.Drawing.Point(161, 120);
+            this.min_time_between_squad_shots_upper_bound.Location = new System.Drawing.Point(151, 38);
             this.min_time_between_squad_shots_upper_bound.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -212,7 +187,7 @@
             // max_time_suspicious_reaction_loop
             // 
             this.max_time_suspicious_reaction_loop.DecimalPlaces = 3;
-            this.max_time_suspicious_reaction_loop.Location = new System.Drawing.Point(161, 48);
+            this.max_time_suspicious_reaction_loop.Location = new System.Drawing.Point(151, 38);
             this.max_time_suspicious_reaction_loop.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -225,7 +200,7 @@
             // min_time_suspicious_reaction_loop
             // 
             this.min_time_suspicious_reaction_loop.DecimalPlaces = 3;
-            this.min_time_suspicious_reaction_loop.Location = new System.Drawing.Point(24, 48);
+            this.min_time_suspicious_reaction_loop.Location = new System.Drawing.Point(14, 38);
             this.min_time_suspicious_reaction_loop.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -238,7 +213,7 @@
             // stealth_light_meter_full_light_threshold
             // 
             this.stealth_light_meter_full_light_threshold.DecimalPlaces = 3;
-            this.stealth_light_meter_full_light_threshold.Location = new System.Drawing.Point(277, 125);
+            this.stealth_light_meter_full_light_threshold.Location = new System.Drawing.Point(14, 116);
             this.stealth_light_meter_full_light_threshold.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -251,7 +226,7 @@
             // stealth_light_meter_full_dark_threshold
             // 
             this.stealth_light_meter_full_dark_threshold.DecimalPlaces = 3;
-            this.stealth_light_meter_full_dark_threshold.Location = new System.Drawing.Point(277, 86);
+            this.stealth_light_meter_full_dark_threshold.Location = new System.Drawing.Point(14, 77);
             this.stealth_light_meter_full_dark_threshold.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -264,7 +239,7 @@
             // stealth_light_meter_timeout_when_detected
             // 
             this.stealth_light_meter_timeout_when_detected.DecimalPlaces = 3;
-            this.stealth_light_meter_timeout_when_detected.Location = new System.Drawing.Point(277, 47);
+            this.stealth_light_meter_timeout_when_detected.Location = new System.Drawing.Point(14, 38);
             this.stealth_light_meter_timeout_when_detected.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -274,31 +249,68 @@
             this.stealth_light_meter_timeout_when_detected.Size = new System.Drawing.Size(186, 20);
             this.stealth_light_meter_timeout_when_detected.TabIndex = 366;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.stealth_light_meter_timeout_when_detected);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.stealth_light_meter_full_dark_threshold);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.stealth_light_meter_full_light_threshold);
+            this.groupBox1.Location = new System.Drawing.Point(237, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 148);
+            this.groupBox1.TabIndex = 367;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stealth Light Meter";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.min_time_between_squad_shots_lower_bound);
+            this.groupBox2.Controls.Add(this.min_time_between_squad_shots_upper_bound);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(213, 72);
+            this.groupBox2.TabIndex = 368;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Squads";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.min_time_suspicious_reaction_loop);
+            this.groupBox3.Controls.Add(this.max_time_suspicious_reaction_loop);
+            this.groupBox3.Location = new System.Drawing.Point(12, 90);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(213, 72);
+            this.groupBox3.TabIndex = 369;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Suspicious Items";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.interaction_distance_threshold);
+            this.groupBox4.Controls.Add(this.label34);
+            this.groupBox4.Location = new System.Drawing.Point(12, 168);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(213, 72);
+            this.groupBox4.TabIndex = 370;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Interactions";
+            // 
             // GlobalConstantsEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 215);
-            this.Controls.Add(this.stealth_light_meter_timeout_when_detected);
-            this.Controls.Add(this.stealth_light_meter_full_dark_threshold);
-            this.Controls.Add(this.stealth_light_meter_full_light_threshold);
-            this.Controls.Add(this.max_time_suspicious_reaction_loop);
-            this.Controls.Add(this.min_time_suspicious_reaction_loop);
-            this.Controls.Add(this.min_time_between_squad_shots_upper_bound);
-            this.Controls.Add(this.min_time_between_squad_shots_lower_bound);
-            this.Controls.Add(this.interaction_distance_threshold);
+            this.ClientSize = new System.Drawing.Size(460, 254);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label34);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -313,21 +325,24 @@
             ((System.ComponentModel.ISupportInitialize)(this.stealth_light_meter_full_light_threshold)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.stealth_light_meter_full_dark_threshold)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.stealth_light_meter_timeout_when_detected)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label34;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
@@ -340,5 +355,9 @@
         private System.Windows.Forms.NumericUpDown stealth_light_meter_full_light_threshold;
         private System.Windows.Forms.NumericUpDown stealth_light_meter_full_dark_threshold;
         private System.Windows.Forms.NumericUpDown stealth_light_meter_timeout_when_detected;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
